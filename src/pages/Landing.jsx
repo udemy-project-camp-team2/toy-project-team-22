8bc3c809d385a4f8a39f0c4b9e232828923c7375
definitely92 <132203871+definitely92@users.noreply.github.com>
2023-07-06 17:57:23 +0900

    landing page commit

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import styles from '../styles/Landing.module.css'
+import LogoImg from '../images/logo.png'
+import Barista from '../components/icons/BaristaImg'
+import { useNavigate } from "react-router-dom";
+
+
 
 const Landing = () => {
-  return <div>This is Landing Page</div>;
+  const navigate = useNavigate();
+  return (
+    <div className={styles.landing}>
+      <h1 className={styles.logo}>
+        Give MY__<br/>Seat
+        <img src={LogoImg} />
+      </h1>
+      <span className={styles.btnLogin} onClick={() => { navigate('./login')}}>
+        카카오 로그인으로 시작하기
+      </span>
+      <div className={styles.barista}>
+        <Barista/>
+      </div>
+      
+    </div>
+  )
 };
 
 export default Landing;
